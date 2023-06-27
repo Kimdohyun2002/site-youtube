2d18fede9f598d1f2624642554aded92900dfd02
Kimdohyun2002 <kshkmn1@gmail.com>
2023-06-27 09:31:14 +0900

    first commit

---
+++ src/utils/content.js
@@ -0,0 +1,30 @@
+import { Tb3DCubeSphere } from 'react-icons/tb';
+import { TbApi } from 'react-icons/tb';
+import { DiApple } from 'react-icons/di';
+import { DiAppstore } from 'react-icons/di';
+import { DiBingSmall } from 'react-icons/di';
+import { DiChrome } from 'react-icons/di';
+
+export const categories = [
+  { name: '웹스토리보이', icon: <Tb3DCubeSphere /> },
+  { name: '코딩애플', icon: <DiApple /> },
+  { name: '좋코딩', icon: <TbApi /> },
+  { name: '노마드 코더', icon: <DiAppstore /> },
+  { name: '조코딩', icon: <DiBingSmall /> },
+  { name: '생활코딩', icon: <DiChrome /> },
+  { name: '드림코딩', icon: <Tb3DCubeSphere /> },
+  { name: '나도코딩', icon: <Tb3DCubeSphere /> },
+  { name: '워니코딩', icon: <Tb3DCubeSphere /> },
+  { name: '얄팍한 코딩사전', icon: <Tb3DCubeSphere /> },
+  { name: '초보코딩', icon: <Tb3DCubeSphere /> },
+  { name: '코딩하는거니', icon: <Tb3DCubeSphere /> },
+  { name: '임효성', icon: <Tb3DCubeSphere /> },
+  { name: '코딩독학 Codok', icon: <Tb3DCubeSphere /> },
+  { name: '동빈나', icon: <Tb3DCubeSphere /> },
+  { name: '코딩셰프', icon: <Tb3DCubeSphere /> },
+  { name: '코딩악마', icon: <Tb3DCubeSphere /> },
+  { name: '윤인성', icon: <Tb3DCubeSphere /> },
+  { name: '캡틴판교', icon: <Tb3DCubeSphere /> },
+  { name: '양재동 코드랩', icon: <Tb3DCubeSphere /> },
+  { name: '코지코더', icon: <Tb3DCubeSphere /> },
+];
